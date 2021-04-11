ce7c30c515775bf0c60507fba0acd9a52ca17fff
caesar-chin <56019561+caesar-chin@users.noreply.github.com>
2021-04-11 00:37:28 -0400

    first commit

---
+++ src/Component/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+import ZipCode from "./ZipCode";
+import City from "./City";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Redirect to="/zipcode" />
+          </Route>
+          <Route exact path="/zipcode">
+            <ZipCode />
+          </Route>
+          <Route exact path="/city">
+            <City />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+}
---
+++ src/Component/City.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import styles from "../Styles/CityStyles.module.css";
+
+export default class City extends Component {
+  render() {
+    return (
+      <div>
+        <div className={styles.title}>
+          <h1 className={styles.titleText}>City Lookup</h1>
+        </div>
+
+        <nav className={styles.navbar}>
+          <div className={styles.navbarDiv}>
+            <Link to={"/zipcode"} className={styles.navbarLinkZipCode}>
+              <div className={styles.navbarItem}>
+                <li>Zip Code Lookup</li>
+              </div>
+            </Link>
+          </div>
+
+          <div className={styles.navbarDiv}>
+            <Link to={"/city"} className={styles.navbarLinkCity}>
+              <div className={styles.navbarItem}>
+                <li>City Lookup</li>
+              </div>
+            </Link>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
