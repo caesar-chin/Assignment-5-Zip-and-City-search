91455b073816ea654cf51b47c72cf4738fe01a2e
Justin Kuang <justin.kuang63@myhunter.cuny.edu>
2021-04-15 15:47:35 -0400

    Create City and ZipSearchField components

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,31 +1,51 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
   Redirect,
-} from "react-router-dom";
-import ZipCode from "./ZipCode";
-import City from "./City";
+} from 'react-router-dom';
+import ZipCode from './ZipCode';
+import City from './City';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      cities: [],
+      zipCode: '',
+    };
   }
+
+  zipChanged = (event) => {
+    let zipCode = event.target.value;
+    if (zipCode.length === 5) {
+      fetch(`http://ctp-zip-api.herokuapp.com/${zipCode}`)
+        .then((res) => res.json())
+        .then((json) => {
+          this.setState({ cities: json });
+        })
+        .catch((err) => {
+          console.log(err);
+          this.setState({ cities: [] });
+        });
+    } else {
+      this.setState({ cities: [] });
+    }
+  };
 
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/">
-            <Redirect to="/zipcode" />
+          <Route exact path='/'>
+            <Redirect to='/zipcode' />
           </Route>
-          <Route exact path="/zipcode">
+          <Route exact path='/zipcode'>
             <ZipCode />
           </Route>
-          <Route exact path="/city">
+          <Route exact path='/city'>
             <City />
           </Route>
         </Switch>
--- src/Component/ZipCode.js
+++ src/Component/ZipCode.js
@@ -1,6 +1,29 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import styles from "../Styles/ZipCodeStyles.module.css";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import styles from '../Styles/ZipCodeStyles.module.css';
+
+function City({ data }) {
+  return (
+    <div>
+      <ul>
+        <li>{data.City}</li>
+      </ul>
+    </div>
+  );
+}
+
+function ZipSearchField({ zipCode, onChange }) {
+  return (
+    <div>
+      <input
+        type='text'
+        value={zipCode}
+        placeholder='Enter Zip Code'
+        onChange={onChange}
+      ></input>
+    </div>
+  );
+}
 
 export default class ZipCode extends Component {
   render() {
@@ -12,15 +35,16 @@ export default class ZipCode extends Component {
 
         <nav className={styles.navbar}>
           <div className={styles.navbarDiv}>
-            <Link to={"/zipcode"} className={styles.navbarLinkZipCode}>
+            <Link to={'/zipcode'} className={styles.navbarLinkZipCode}>
               <div className={styles.navbarItem}>
-                <li>Zip Code Lookup</li>
+                <li>Zip Code Lookup:</li>
+                <ZipSearchField />
               </div>
             </Link>
           </div>
 
           <div className={styles.navbarDiv}>
-            <Link to={"/city"} className={styles.navbarLinkCity}>
+            <Link to={'/city'} className={styles.navbarLinkCity}>
               <div className={styles.navbarItem}>
                 <li>City Lookup</li>
               </div>
